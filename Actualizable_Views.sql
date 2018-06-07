474f794ad35946f280735006def9923fa0e582c4
JoseMeseguer <39100184+JoseMeseguer@users.noreply.github.com>
2018-06-07 17:35:47 +0200

    Add files via upload

--- Actualizable_Views.sql
+++ Actualizable_Views.sql
@@ -1,5 +1,6 @@
 set search_path to tests;
 
+-- DOS TABLAS CON INFORMACION SOBRE CLIENTES PERO QUE PUEDEN CONSULTARSE INDEPENDIENTEMENTE
 --drop  table userweb cascade
 create table userweb (
     id  bigserial primary key,      --tipo especial, autonumerico de 8 bytes
@@ -10,6 +11,7 @@ create table userweb (
 	status int	-- 0 = registered, 1 = member, 2 = premium
 );
 create unique index uniquenick on userweb(nick);
+-- CREAMOS UN INDICE PARA QUE NO SE REPITA EL NICK DEL USUARIO
 
 --drop table client
 create table client (
@@ -19,44 +21,59 @@ create table client (
     address   varchar(35) not null
 );
 
+-- VISTA AUTOMATICAMENTE ACTUALIZABLE SOBRE USERWEB
 create or replace view usersweb as 
 select nick, password, email, dateuser, status
 from userweb;
 
+-- VISTA QUE "FUSIONA" AMBAS TABLAS PERO QUE NO ES AUTOMATICAMENTE ACTUALIZABLE
 create or replace view users as 
 select name, nick, password, dateuser, phone, address, email, status
 from userweb left join client USING (id);
 
+-- CONJUNTO DE SELECTS SOBRE TABLAS Y VISTAS
 select * from userweb;
 select * from client;
 select * from usersweb;
 select * from users;
 
+-- OPERACIONES DE ESCRITURA DIRECTAMENTE SOBRE LA VISTA ACTUALIZABLE
 insert into usersweb values ('john2', '1234', 'john@gmail.com', current_timestamp, 0);
 insert into usersweb values ('jim', '1234', 'jim@gmail.com', current_timestamp, 0);
 update usersweb set status = 1 where nick = 'john2';
 delete from usersweb where nick = 'jim';
 
+
+-- VISTA SOBRE USERWEB CON UNA CONDICION, PERO QUE SIN CREAR UNA REGLA PARA COMPROBAR DICHA CONDICION
 create or replace view vipsusers as 
 select nick, password, email, dateuser, status
 from userweb where status <> 0;
 
 select * from vipsusers;
-insert into vipsusers values ('luke3', '1234', 'luke@gmail.com', current_timestamp, 1);
+-- PERMITE INSERTAR Y MODIFICAR REGISTROS QUE DESPUES YA NO PODREMOS VER
+insert into vipsusers values ('luke3', '1234', 'luke@gmail.com', current_timestamp, 0);
 update vipsusers set status = 0 where nick = 'john2';
 update vipsusers set status = 1 where nick = 'john';   -- NO NOS PERMITIRA MODIFICAR REGISTROS QUE NO APARECEN EN LA VISTA
 
+
+-- OBLIGAMOS A QUE LA VISTA COMPRUEBE LA CONDICION
 create or replace view vipsusers as 
 select nick, password, email, dateuser, status
 from userweb where status <> 0
 WITH CASCADED CHECK OPTION;
+-- YA NO SE NOS PERMITIRAN MODIFICACIONES QUE IMPLICARAN QUE LOS DATOS QUEDEN FUERA DE LA VISTA
+
 
 
 select * from users;
+-- COMPROBACION DE QUE LA VISTA USERS NO ES AUTOMATICAMENTE ACTUALIZABLE
 insert into users values ('luke', '1234', 'luke@gmail.com', current_timestamp, 0);
 update users set status = 0 where nick = 'john';
 
 
+
+-- PARA IMPLEMENTAR QUE SEA ACTUALIZABLE CREAMOS LA FUNCION A LA QUE LLAMARA EL 
+-- TRIGGER INSTEAD OF
 CREATE OR REPLACE FUNCTION updateusers()  RETURNS TRIGGER AS $$
 DECLARE ref bigint;
 BEGIN
@@ -82,10 +99,14 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
+-- TRIGGER QUE LLAMARA A LA FUNCION PARA QUE EJECUTE SUS COMANDO EN LUGAR DE LOS QUE 
+-- HA REALIZADO EL USUARIO
 --DROP TRIGGER  trg_updateusers  ON users
 CREATE TRIGGER trg_updateusers INSTEAD OF INSERT OR UPDATE OR DELETE ON users
 FOR EACH ROW EXECUTE PROCEDURE updateusers();
 
+
+-- COMPROBACION DE QUE LA VISTA YA ES ACTUALIZABLE A TRAVES DEL TRIGGER
 select * from users;
 insert into users values ('Lucas Martinez', 'luke', '1234', current_timestamp, '666666666',
 						'street 6', 'luke@gmail.com', 0 );
